fefc8b9318a80ff443940ffbc5cd936f0d59f6f6
Nischal <tamangnischal2018@gmail.com>
2024-10-26 19:18:37 +0545

    force redirected to home page

--- src/home.jsx
+++ src/home.jsx
@@ -5,7 +5,7 @@ import { SignIn, SignInButton } from '@clerk/clerk-react'
 const Home = () => {
   return (
     <div>
-        <SignInButton mode='modal' >
+        <SignInButton mode='modal' forceRedirectUrl='/' >
      <Button>Sign in</Button>
      </SignInButton>
     </div>
