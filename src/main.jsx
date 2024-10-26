3b13430d88d7c532a5bf3b822d83e585797c45fd
Nischal <tamangnischal2018@gmail.com>
2024-10-26 18:44:20 +0545

    creating contact and home page

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './home'
+import Contact from './contact'
+
+
+const router = createBrowserRouter([
+{
+  path:'/',
+  element:<Home/>
+},
+{
+  path:'/contact',
+  element:<Contact/>
+}
+])
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+   <RouterProvider router={router}/>
   </StrictMode>,
 )
