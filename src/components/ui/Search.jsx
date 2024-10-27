860d71107d5142d7ceaf3a81c565f1636076745f
Nischal <tamangnischal2018@gmail.com>
2024-10-27 16:40:05 +0545

    installed select from shadcn ui

--- src/components/ui/Search.jsx
+++ src/components/ui/Search.jsx
@@ -1,4 +1,12 @@
 import React from 'react'
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+  } from "@/components/ui/select"
+  
 
 function Search() {
   return (
