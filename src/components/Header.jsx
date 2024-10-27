1d138c097fce2fa681c6a73fd66b59cdc94a0c9e
Nischal <tamangnischal2018@gmail.com>
2024-10-27 16:03:20 +0545

    made button for user signin

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,10 @@
+import { UserButton, useUser } from '@clerk/clerk-react'
 import React from 'react'
+import { Button } from './ui/button'
 
 function Header() {
+  const {user, isSignedIn} = useUser()
+
   return (
     <div>
       <img src='/logo.svg' width={150} height={100}/>
@@ -10,6 +14,14 @@ function Header() {
         <li>New</li>
         <li>Preowned</li>
       </ul>
+      {isSignedIn? 
+      
+    <div>
+      <UserButton/>
+      <Button>Submit Listing</Button>
+    </div> :
+    <Button>Submit Listing</Button>
+    }
     </div>
   )
 }
