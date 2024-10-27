d0ca26fc23207f65e9c06da5a5c2873bd2f2f685
Nischal <tamangnischal2018@gmail.com>
2024-10-27 16:48:44 +0545

    making search bar in hero section

--- src/components/ui/Search.jsx
+++ src/components/ui/Search.jsx
@@ -1,19 +1,47 @@
-import React from 'react'
+import React from "react";
 import {
-    Select,
-    SelectContent,
-    SelectItem,
-    SelectTrigger,
-    SelectValue,
-  } from "@/components/ui/select"
-  
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 function Search() {
   return (
-    <div>
-      
+    <div className="p-2 md:p-5 bg-white rounded-md md:rounded-full flex-col md:flex md:flex-row gap-10 px-5 items-center w-full md:w-max">
+      <Select>
+        <SelectTrigger className="w-[180px] outline-none md:border-none w-full shadow-none text-lg">
+          <SelectValue placeholder="Theme" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="light">Light</SelectItem>
+          <SelectItem value="dark">Dark</SelectItem>
+          <SelectItem value="system">System</SelectItem>
+        </SelectContent>
+      </Select>
+      <Select>
+        <SelectTrigger className="w-[180px] outline-none md:border-none w-full shadow-none text-lg">
+          <SelectValue placeholder="Theme" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="light">Light</SelectItem>
+          <SelectItem value="dark">Dark</SelectItem>
+          <SelectItem value="system">System</SelectItem>
+        </SelectContent>
+      </Select>
+      <Select>
+        <SelectTrigger className="w-[180px] outline-none md:border-none w-full shadow-none text-lg">
+          <SelectValue placeholder="Theme" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="light">Light</SelectItem>
+          <SelectItem value="dark">Dark</SelectItem>
+          <SelectItem value="system">System</SelectItem>
+        </SelectContent>
+      </Select>
     </div>
-  )
+  );
 }
 
-export default Search
+export default Search;
