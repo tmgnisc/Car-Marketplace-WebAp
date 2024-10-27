6f262da1d5b90cd9680488e218f4f0d8c6a06ad9
Nischal <tamangnischal2018@gmail.com>
2024-10-27 16:36:37 +0545

    making hero section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <div>
+      <div className='flex flex-col items-center p-10 py-20 gap-6 h-[600px] w-full bg-[#eef0fc]'>
+        <h2 className='text-lg'>Find cars for sale and for rent near you.</h2>
+        <h2 className='text-[60px] font-bold'>Find your Dream car.</h2>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
--- src/home.jsx
+++ src/home.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 
 import Header from './components/Header'
+import Hero from './components/Hero'
 
 const Home = () => {
   return (
     <div>
       {/* Header */}
       <Header/>
+      {/* hero */}
+      <Hero/>
     </div>
   )
 }
