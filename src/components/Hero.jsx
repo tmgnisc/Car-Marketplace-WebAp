05392919e1cc723d59004f451fae550151cea844
Nischal <tamangnischal2018@gmail.com>
2024-10-27 16:38:22 +0545

    creating search component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Search from './ui/Search'
 
 function Hero() {
   return (
@@ -6,6 +7,7 @@ function Hero() {
       <div className='flex flex-col items-center p-10 py-20 gap-6 h-[600px] w-full bg-[#eef0fc]'>
         <h2 className='text-lg'>Find cars for sale and for rent near you.</h2>
         <h2 className='text-[60px] font-bold'>Find your Dream car.</h2>
+        <Search/>
       </div>
     </div>
   )
