6fe6235b846312a2f3f7b05269a173a1125c03f1
Nischal <tamangnischal2018@gmail.com>
2024-10-28 19:31:39 +0545

    most searched car component completed

--- src/components/CarItem.jsx
+++ src/components/CarItem.jsx
@@ -8,30 +8,38 @@ import { MdOpenInNew } from "react-icons/md";
 function CarItem({ car }) {
   return (
     <div className="rounded-xl bg-white border hover:shadow-lg cursor-pointer">
-        <h2 className="absolute m-2 bg-green-500 px-2 rounded-full text-sm text-white pb-1">New</h2>
-      <img src={car?.image} width={'100%'} height={250} className="rounded-t-xl" />
+      <h2 className="absolute m-2 bg-green-500 px-2 rounded-full text-sm text-white pb-1">
+        New
+      </h2>
+      <img
+        src={car?.image}
+        width={"100%"}
+        height={250}
+        className="rounded-t-xl"
+      />
       <div className="p-4">
         <h2 className="font-bold text-black text-lg mb-2">{car?.name}</h2>
         <Separator />
-        <div className="grid grid-cols-3 mt-5"> 
+        <div className="grid grid-cols-3 mt-5">
           <div className="flex flex-col items-center">
             <LuFuel className="text-lg mb-2" />
             <h2>{car.miles}miles</h2>
           </div>
           <div className="flex flex-col items-center">
-          <SiSpeedtest className="text-lg mb-2" />
-          <h2>{car.fuelType}</h2>
+            <SiSpeedtest className="text-lg mb-2" />
+            <h2>{car.fuelType}</h2>
           </div>
-          <div className="flex flex-col items-center"> 
-          <FaGear className="text-lg mb-2" />
-          <h2>{car.gearType}</h2>
+          <div className="flex flex-col items-center">
+            <FaGear className="text-lg mb-2" />
+            <h2>{car.gearType}</h2>
           </div>
         </div>
-        <Separator className="my-2"/>
+        <Separator className="my-2" />
         <div className="flex items-center justify-between">
-            <h2 className="font-bold text-xl">Nrs.{car.price}</h2>
-            <h2 className="text-primary text-sm flex gap-2 items-center"> 
-            View Details <MdOpenInNew /></h2>
+          <h2 className="font-bold text-xl">Nrs.{car.price}</h2>
+          <h2 className="text-primary text-sm flex gap-2 items-center">
+            View Details <MdOpenInNew />
+          </h2>
         </div>
       </div>
     </div>
