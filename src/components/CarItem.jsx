24fd2c5f691bac41957f35891fde78ed45dcabe7
Nischal <tamangnischal2018@gmail.com>
2024-10-28 19:15:38 +0545

    making car item section

--- src/components/CarItem.jsx
+++ src/components/CarItem.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+  
 function CarItem({car}) {
   return (
     <div>
--- src/components/MostSearchedCar.jsx
+++ src/components/MostSearchedCar.jsx
@@ -1,20 +1,31 @@
-import React from 'react'
-import FakeData from '@/Shared/FakeData'
-import CarItem from './CarItem'
-
+import React from "react";
+import FakeData from "@/Shared/FakeData";
+import CarItem from "./CarItem";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 function MostSearchedCar() {
   return (
-   <div>
-    <h2 className='font-bold text-3xl text-center my-8'>Most Searched Car</h2>
-
-    {FakeData.carList.map((car,index)=>(
-        <CarItem car={car}/>
-    ))}
-   </div>
-    
- 
-    
-  )
+    <div className="mx-24">
+      <h2 className="font-bold text-3xl text-center my-8">Most Searched Car</h2>
+      <Carousel>
+        <CarouselContent>
+          {FakeData.carList.map((car, index) => (
+            <CarouselItem className="basis-1/4">
+              {" "}
+              <CarItem car={car} />
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+    </div>
+  );
 }
 
-export default MostSearchedCar
+export default MostSearchedCar;
