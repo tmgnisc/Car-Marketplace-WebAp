d18ca68e21f953548b930b54e48bbed004e6268c
Nischal <tamangnischal2018@gmail.com>
2024-10-28 19:32:06 +0545

    most searched car component completed

--- src/Shared/FakeData.jsx
+++ src/Shared/FakeData.jsx
@@ -1,20 +1,21 @@
-import {faker} from '@faker-js/faker'
-function createRandomCarList(){
-    return {
-        name:faker.vehicle.vehicle(),
-        fuelType:faker.vehicle.fuel(),
-        model:faker.vehicle.model(),
-        type:faker.vehicle.type(),
-        image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQef9NspjZwWA3D9DkH-nD5Fau2LD8jG3lnDw&s',
-        miles:1000,
-        gearType:'Automatic',
-        price:faker.finance.amount({min:3000, max:20000})
-    }
+import { faker } from "@faker-js/faker";
+function createRandomCarList() {
+  return {
+    name: faker.vehicle.vehicle(),
+    fuelType: faker.vehicle.fuel(),
+    model: faker.vehicle.model(),
+    type: faker.vehicle.type(),
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQef9NspjZwWA3D9DkH-nD5Fau2LD8jG3lnDw&s",
+    miles: 1000,
+    gearType: "Automatic",
+    price: faker.finance.amount({ min: 3000, max: 20000 }),
+  };
 }
 
-const carList=faker.helpers.multiple(createRandomCarList,{
-    count:7
-})
+const carList = faker.helpers.multiple(createRandomCarList, {
+  count: 7,
+});
 export default {
-    carList
-}
+  carList,
+};
