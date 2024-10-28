6b8b3f8d55979b2f6a7f0d24fc0c0c709e114189
Nischal <tamangnischal2018@gmail.com>
2024-10-28 18:59:48 +0545

    made car item component

---
+++ src/components/CarItem.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CarItem(car) {
+  return (
+    <div>
+      caritem
+    </div>
+  )
+}
+
+export default CarItem
--- src/components/MostSearchedCar.jsx
+++ src/components/MostSearchedCar.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import FakeData from '@/Shared/FakeData'
+import CarItem from './CarItem'
 
 function MostSearchedCar() {
   return (
-    console.log(FakeData.carList)
+   <div>
+    <h2 className='font-bold text-3xl text-center my-8'>Most Searched Car</h2>
+
+    {FakeData.carList.map(({car,index})=>(
+        <CarItem car={car} leu={index}/>
+    ))}
+   </div>
     
  
     
