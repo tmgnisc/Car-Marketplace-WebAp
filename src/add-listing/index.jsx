4ff779dc5c97d3b6e0299926633e31c93564b9db
Nischal <tamangnischal2018@gmail.com>
2024-10-29 11:58:46 +0545

    creating add listing form

--- src/add-listing/index.jsx
+++ src/add-listing/index.jsx
@@ -1,20 +1,21 @@
-import Header from '@/components/Header'
-import React from 'react'
+import Header from "@/components/Header";
+import React from "react";
 
 function AddListing() {
   return (
     <div>
-      <Header/>
-      <div className='px-10 md:px-20 my-10'>
-        <h2 className='font-bold text-4xl'>Add new Listing</h2>
-        <form action="">
-{/* car details */}
-
-
+      <Header />
+      <div className="px-10 md:px-20 my-10">
+        <h2 className="font-bold text-4xl">Add new Listing</h2>
+        <form className="p-10 border rounded-xl mt-10">
+          {/* car details */}
+          <div>
+            <h2 className="font-medium text-xl mb-6">Car Details</h2>
+          </div>
         </form>
       </div>
     </div>
-  )
+  );
 }
 
-export default AddListing
+export default AddListing;
