1db95ca7fa24837adbf97096b2284e5e3fad4527
Nischal <tamangnischal2018@gmail.com>
2024-10-29 11:41:48 +0545

    creating add listing

--- src/profile/index.jsx
+++ src/profile/index.jsx
@@ -6,9 +6,12 @@ function Profile() {
   return (
     <div>
      <Header/>
-     <div>
+     <div className='px-10 md:px-20 my-10'>
+        <div className='flex justify-between items-center'>
         <h2 className='font-bold text-4xl'>My Listing</h2>
         <Button>+ Add New Listing</Button>
+        </div>
+       
      </div>
     </div>
   )
