42335b5555798c26aa83b7f3df2d0adaaa83ea9a
Nischal <tamangnischal2018@gmail.com>
2024-10-29 11:53:42 +0545

    creating add listing form

--- src/add-listing/index.jsx
+++ src/add-listing/index.jsx
@@ -1,9 +1,18 @@
+import Header from '@/components/Header'
 import React from 'react'
 
 function AddListing() {
   return (
     <div>
-      AddListing
+      <Header/>
+      <div className='px-10 md:px-20 my-10'>
+        <h2 className='font-bold text-4xl'>Add new Listing</h2>
+        <form action="">
+{/* car details */}
+
+
+        </form>
+      </div>
     </div>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import { UserButton, useUser } from '@clerk/clerk-react'
 import React from 'react'
 import { Button } from './ui/button'
+import { Link } from 'react-router-dom'
 
 function Header() {
   const {user, isSignedIn} = useUser()
@@ -18,7 +19,8 @@ function Header() {
       
     <div className='flex items-center gap-5'>
       <UserButton/>
-     <Button> Submit Listing </Button>
+      <Link to={'/profile'}> <Button> Submit Listing </Button></Link>
+    
     </div> :
     <Button> Submit Listing </Button>
     }
