e7e6c963b44ecd445c43df5308977d5a3e8ed73a
Nischal <tamangnischal2018@gmail.com>
2024-10-30 11:22:45 +0545

    setting up backend

--- configs/index.js
+++ configs/index.js
@@ -1,4 +1,4 @@
 import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
-const sql = neon(process.env.DATABASE_URL);
-const db = drizzle({ client: sql });
+const sql = neon(import.meta.env.VITE_DRIZZLE_DATABASE_URL);
+export const db = drizzle({ client: sql });
