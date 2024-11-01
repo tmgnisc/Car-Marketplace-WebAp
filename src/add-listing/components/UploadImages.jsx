f6b3b18b4b1c889f16c86361188a894235c0a0a5
Nischal <tamangnischal2018@gmail.com>
2024-11-01 18:44:44 +0545

    added cross button and rendered images preview

--- src/add-listing/components/UploadImages.jsx
+++ src/add-listing/components/UploadImages.jsx
@@ -1,18 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import { IoMdCloseCircle } from "react-icons/io";
 
 function UploadImages() {
-    const [selectedFileList, setSelectedFileList]= useState([])
+  const [selectedFileList, setSelectedFileList] = useState([]);
   const onFileSelected = (event) => {
     const files = event.target.files;
-    for(let i=0; i<files?.length;i++){
-        const file = files[i]
-        setSelectedFileList((prev)=>[...prev,file])
+    for (let i = 0; i < files?.length; i++) {
+      const file = files[i];
+      setSelectedFileList((prev) => [...prev, file]);
     }
   };
   return (
     <div>
       <h2 className="font-medium text-xl my-3">Upload car Images</h2>
-      <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6">
+      <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-5">
+        {selectedFileList.map((image, index) => (
+            <div key={index}>
+                <IoMdCloseCircle/>
+                <img
+            src={URL.createObjectURL(image)}
+            className="w-full h-[130px] object-cover rounded-xl"
+          />
+            </div>
+          
+        ))}
         <label htmlFor="upload-images">
           <div className="border rounded-xl border-dotted border-primary bg-blue-100 p-10 cursor-pointer hover:shadow-lg">
             <h2 className="text-lg text-center text-primary">+</h2>
