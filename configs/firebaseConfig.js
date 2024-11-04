15c484f6f92e7c5ae050787a721e6006771c5b4e
Nischal <tamangnischal2018@gmail.com>
2024-11-04 13:52:50 +0545

    adding config file of firebase

---
+++ configs/firebaseConfig.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "car-marketplace-bfe57.firebaseapp.com",
+  projectId: "car-marketplace-bfe57",
+  storageBucket: "car-marketplace-bfe57.firebasestorage.app",
+  messagingSenderId: "439806093429",
+  appId: "1:439806093429:web:88b3b857ce913631f2b03c",
+  measurementId: "G-NF46YENW3G"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
